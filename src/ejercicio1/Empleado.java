a196ce6b3b5b0ce069f85bd36c33c15daaa6bf13
Facundo Masino <facujmasino@gmail.com>
2024-08-21 22:26:14 -0300

    Agrego Punto 1

---
+++ src/ejercicio1/Empleado.java
@@ -0,0 +1,72 @@
+package ejercicio1;
+
+public class Empleado {
+	
+	// Atributos
+	private static int contadorId = 999;
+	
+	private final int id; 
+	private String nombre;
+	private int edad;
+	
+	//Constructores
+	
+	public Empleado() 
+	{
+		contadorId ++;
+		
+		this.id = contadorId;
+		this.nombre = "sin nombre";
+		this.edad = 99;
+	}
+	
+	public Empleado(String nombre, int edad)
+	{
+		contadorId ++;
+		
+		this.id = contadorId;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	// Getters y setters
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	
+	public String getNombre()
+	{
+		return nombre;
+	}
+	
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+	
+	public int getEdad()
+	{
+		return edad;
+	}
+	
+	public void setEdad(int edad)
+	{
+		this.edad = edad;
+	} 
+	
+	//Punto 6 y 7
+
+	public static int devuelveProximoId()
+	{
+		return contadorId+1;
+	}
+	
+	@Override
+	public String toString() {
+		return "Empleado: ID= "+ id +", Nombre= " + nombre + ", Edad= " + edad;
+	}
+	
+}
---
+++ src/ejercicio1/Profesor.java
@@ -0,0 +1,6 @@
+package ejercicio1;
+
+public class Profesor extends Empleado {
+	String cargo;
+	int antiguedadDocente;
+}
