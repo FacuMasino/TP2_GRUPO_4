de2a48d5708620b14e0eaa0807b015eacfe35bd5
JuaneloDev <juancromero91@outlook.com>
2024-08-23 21:49:55 -0300

    Puntos 2, 3, 4 y 5
    Encapsulamiento, constructores utilizando super, método toString, cargar arrayList y recorrerlo con un ListIterator

---
+++ src/ejercicio1/MainEjercicio1_a.java
@@ -0,0 +1,30 @@
+package ejercicio1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MainEjercicio1_a {
+
+	public static void main(String[] args) {
+		
+		//Punto 4
+		List<Profesor> listaProfesores = new ArrayList<>();
+		
+		listaProfesores.add(new Profesor("Juan", 33, "Profesor", 5));
+		listaProfesores.add(new Profesor("Gonzalo", 31, "Director", 9));
+		listaProfesores.add(new Profesor("Maximiliano", 30, "Rector", 7));
+		listaProfesores.add(new Profesor("Ana", 29, "Profesora", 3));
+		listaProfesores.add(new Profesor("Facundo", 25, "Profesor", 4));
+		
+		ListIterator<Profesor> iterarProfesores = listaProfesores.listIterator();
+		while(iterarProfesores.hasNext()) {
+			
+			Profesor profesor = iterarProfesores.next();
+			System.out.println(profesor.toString());
+			
+		}
+		
+	}
+
+}
--- src/ejercicio1/Profesor.java
+++ src/ejercicio1/Profesor.java
@@ -1,6 +1,48 @@
 package ejercicio1;
 
 public class Profesor extends Empleado {
-	String cargo;
-	int antiguedadDocente;
+	
+	// Atributos
+	private String cargo;
+	private int antiguedadDocente;
+	
+	//Constructores
+	public Profesor() {
+		
+		super();
+	};
+	
+	public Profesor(String nombre, int edad, String cargo, int antiguedad) {
+		
+		super(nombre, edad);
+		this.cargo = cargo;
+		this.antiguedadDocente = antiguedad;
+	}
+	
+	// Getters y setters
+	public String getCargo() {
+		return cargo;
+	}
+	
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+	
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
+
+	//Punto 4
+	@Override
+	public String toString() {
+		
+		return super.toString() + ", Cargo= " + cargo + ", Antiguedad=" + antiguedadDocente;
+	}
+	
+	
+	
 }
